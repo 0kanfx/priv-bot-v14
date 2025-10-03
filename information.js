cc152c7475ed7c5de4a9bb5135dd836f12671500
schwest <169075683+0kanfx@users.noreply.github.com>
2025-10-03 17:56:11 +0300

    Create information.js

---
+++ information.js
@@ -0,0 +1,44 @@
+<span style="color: red;">Yeni Nesil V14 Private Botları: </span> 
+
+Genel İçerik:
+1 Adet Supervisor 
+1 Adet Statistics 
+1 Adet Register
+1 Adet Controller 
+3 Adet Guard
+Sınırsız Adet Welcomer 
+
+Özel Sistemler:
+
+Özel Oda Sistemi,
+Aylık Haftalık Top İstatistik Sistemi,
+Leaderboard sistemi.,
+Özel Rol Komutları (Vip, streamer, ytver vb.),
+Aylık, haftalık stat sıfırlanma (devridaim stat),
+Mesaj, Ses, Kamera, Yayın, Davet, Kayıt İstatistik Sistemi, 
+Kayıt Komutları (erkek, kadın, unregister, teyitler),
+Davet takip sistemi (join, left)
+Ceza sistemi (Ban, Jail, Cmute, Vmute vb),
+Ceza takip sistemi,
+Ceza puan sistemi, 
+Setup Sistemi (Basit Düzey),
+Setuplu Guard Sistemi,
+Limitli Guard Sistemi,
+Gelişmiş Guard Komutları, 
+Url, webhook, web, offline, kanal, rol vs korumaları,
+Açma kapamalı sistemler (stat, kayıt, tag sistemi),
+Çift tag sistemi (İster tagsız ister ekip tagı kullanabilirsiniz),
+Blacklist sistemi (cmdban mantığı),
+Gelişmiş başvuru panelleri (Öneri, Şikayet, Streamer, Yetkili),
+Gelişmiş kısayollar paneli,
+Sıfırlama komutları (Stat, Ceza-Puan, Ceza Geçmişi, Kayıt),
+Rol-Alım panelleri (Burç, ilişki, takım, oyun vb),
+Tweet-Ship (Gelişmiş tweet paneli ve ship komutu)
+Ceza-Sorgu, Davet-Sorgu, Profil, Zengin ve daha birçok kullanıcı komutu.
+
+Tüm komutlar çok fonksiyonlu ve gelişmiştir. Öyle absürt komutlar değil 5 satırlık değil yani xd
+Sunucu içi tüm olaylar (kamera açılıp kapanması, yayın etkinlikleri, ses giriş çıkışları, silinen düzenlenen mesajlar vs tüm etkinlikler tek tek loglanır.)
+Botu ister .setup komutu ile isterseniz de settings.json dosyasından elle kurabilirsiniz
+Tüm sistemler özenilmiştir ve sürekli iyileştirilmelere gidilmiştir. 
+
+Güzel bi star gelmesi halinde bu botu paylaşabilirim. Ücretsiz bir şekilde bu botu elde edebilirsiniz.
